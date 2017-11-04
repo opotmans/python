5c2c35cad45c5db6e0216d4cf9e7177f83a85b2e
opotmans <potmans@hotmail.com>
2017-11-04 20:32:03 +0100

    dataframe

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -15,3 +15,9 @@ print (dates)
 
 temp1 = pd.Series([80,81,82,83],index=dates)
 print (temp1)
+temp2= pd.Series ([70,71,72,73],index=dates)
+temps_diff = temp1-temp2
+print (temps_diff)
+print (temps_diff.mean())
+temps_df = pd.DataFrame({'brussels' : temp1, 'namur': temp2})
+print (temps_df)
