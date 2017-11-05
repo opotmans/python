fdadb0479f555fc2a541cdf49ac814d89b4ec2c7
opotmans <potmans@hotmail.com>
2017-11-05 18:31:17 +0100

    series with np

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -15,8 +15,18 @@ pd.set_option('display.width',200)
 #===================================
 
 #create a serie of number
+#using a python list
 s1 = pd.Series ([1,2,3,4])
 print(s1)
+#using numpy 
+s2 = pd.Series (np.arange(4,9))
+print (s2)
+#using random number of numpy
+np.random.seed(12345)
+s3 = pd.Series(np.random.normal(size=5))
+print(s3)
+
+
 
 #Create a serie of times with start and end, possible to define the frequencies
 dates = pd.date_range ('2017-04-01','2017-04-04')
@@ -34,8 +44,9 @@ print (temps_df)
 #Create a dataframe based on a CSV : Extract file of csv
 # on the file marx-geo.csv there is fields number error ==> I use the option error_bad_lines=False to bypass these errors
 # Seeing that this is a timeseries, I use the first column as datetime as the index: this is the option index_col='datetime'
+# https://pandas.pydata.org/pandas-docs/stable/generated/pandas.read_csv.html
 marx = pd.read_csv ('c:/users/opotmans/Downloads/marx-geo.csv',error_bad_lines=False,index_col='datetime')
 print(marx.head())
 #to display the index of the dataframe (marx)
 print(marx.index)
-
+print (marx.)
