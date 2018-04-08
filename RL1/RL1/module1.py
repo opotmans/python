0a4ab2f16051bfb94568d60fc2bfb6d6663b9095
opotmans <potmans@hotmail.com>
2018-04-08 11:45:50 +0200

    dfbd

--- RL1/RL1/module1.py
+++ RL1/RL1/module1.py
@@ -1,3 +1,4 @@
+
 import gym
 env = gym.make('CartPole-v0')
 for i_episode in range(20):
@@ -9,4 +10,4 @@ for i_episode in range(20):
         observation, reward, done, info = env.step(action)
         if done:
             print("Episode finished after {} timesteps".format(t+1))
-            break 
+            break
