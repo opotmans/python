cf75b0b58805349cf6fa535462b99e617154f62e
potmans <potmans@hotmail.com>
2024-10-12 12:32:48 +0000

    documents implemented

--- RAG_test1.py
+++ RAG_test1.py
@@ -1,5 +1,6 @@
-#ceci est un test pour implémenter un système de RAG
+#from llama_index.llms.ollama import Ollama
 from llama_index.core import SimpleDirectoryReader
 from llama_index.core.ingestion import IngestionPipeline
 from llama_index.core.node_parser import TokenTextSplitter
 documents = SimpleDirectoryReader("/root/data").load_data()
+print(documents)
