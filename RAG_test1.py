591903071988a21e8ddb930be3a5229c9a3b65f4
potmans <potmans@hotmail.com>
2024-10-12 12:17:12 +0000

    import libraries

--- RAG_test1.py
+++ RAG_test1.py
@@ -1,1 +1,5 @@
 #ceci est un test pour implémenter un système de RAG
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core.ingestion import IngestionPipeline
+from llama_index.core.node_parser import TokenTextSplitter
+documents = SimpleDirectoryReader("/root/data").load_data()
