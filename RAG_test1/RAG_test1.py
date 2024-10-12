607d56a884e94b9dd3eee92d3b1c1dfe99ab6364
potmans <potmans@hotmail.com>
2024-10-12 12:46:50 +0000

    adapt

--- RAG_test1/RAG_test1.py
+++ RAG_test1/RAG_test1.py
@@ -1,4 +1,4 @@
-#from llama_index.llms.ollama import Ollama
+#from llama_index.llms.ollama import Ollama xxx
 from llama_index.core import SimpleDirectoryReader
 from llama_index.core.ingestion import IngestionPipeline
 from llama_index.core.node_parser import TokenTextSplitter
