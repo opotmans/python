ea8694850c685666564540d4168c311e2e8f3167
potmans <potmans@hotmail.com>
2024-10-17 12:48:55 +0000

    adaptation qdrant

--- RAG_test/Test_ollama.py
+++ RAG_test/Test_ollama.py
@@ -5,7 +5,9 @@ from llama_index.core import VectorStoreIndex
 from llama_index.core.node_parser import TokenTextSplitter
 from llama_index.llms.ollama import Ollama
 
-
+import os
+os.environ ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+print ("test")
 # Load the document for the creation of the knowledge based"
 documents = SimpleDirectoryReader("/root/data").load_data()
 #print(documents)
@@ -14,16 +16,24 @@ documents = SimpleDirectoryReader("/root/data").load_data()
 # Option 1 : use the huggingface requires the installation of the modules
 # download le modèle bert sur huggingface
 #test
+print("enter into Hugging")
 
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 embed_model = HuggingFaceEmbedding()
 
+
 #Indexing and storing embedding on disk
 #import llama_index.vector_stores.qdrant import QdrantVectorStore
-from llama_index.vector_Stores.qdrant import QdrantVectorStore
+#from six import _thread
+
+print("enter into qdrant")
+from llama_index.vector_stores.qdrant import QdrantVectorStore
 import qdrant_client
 
-client = qdrant_client.QdrantClient(url="http://172.17.0.3:6333")
+client = qdrant_client.QdrantClient(
+    host = "172.22.208.167"
+    port = 6333
+    )
 
 vector_store = QdrantVectorStore(client=client, collection_name="RAG_test")
 #import chromadb
