4f316ea9c3d0eab17e27040bfd917e3bbf5dc0c8
potmans <potmans@hotmail.com>
2024-10-17 18:22:21 +0000

    create instead of recreate

--- RAG_test/Test_ollama.py
+++ RAG_test/Test_ollama.py
@@ -38,7 +38,7 @@ client = qdrant_client.QdrantClient(
     port = 6333
     )
 print ("recreate")
-client.recreate_collection(
+client.create_collection(
         collection_name="RAG_test",
         vectors_config=qmodels.VectorParams(
             size = 384,
