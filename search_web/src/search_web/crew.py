0ecbebf9776d2ddf8fafb48338ba1d2edbadd2e1
potmans <potmans@hotmail.com>
2024-12-01 20:25:14 +0000

    crewai-Search_web

---
+++ search_web/src/search_web/crew.py
@@ -0,0 +1,107 @@
+from crewai import Agent, Crew, Process, Task, LLM
+from crewai.project import CrewBase, agent, crew, task, before_kickoff, after_kickoff
+from crewai_tools import SerperDevTool,ScrapeWebsiteTool,FileWriterTool
+from dotenv import load_dotenv
+
+# Uncomment the following line to use an example of a custom tool
+# from search_web.tools.custom_tool import MyCustomTool
+load_dotenv()
+
+# Check our tools documentations for more information on how to use them
+# from crewai_tools import SerperDevTool
+
+@CrewBase
+class SearchWeb():
+	"""SearchWeb crew"""
+
+	agents_config = 'config/agents.yaml'
+	tasks_config = 'config/tasks.yaml'
+
+	ollama_llm =LLM (
+		model ='ollama/llama3.2:latest',
+		base_url="http://localhost:11434"
+	)
+
+	@before_kickoff # Optional hook to be executed before the crew starts
+	def pull_data_example(self, inputs):
+		# Example of pulling data from an external API, dynamically changing the inputs
+		inputs['extra_data'] = "This is extra data"
+		return inputs
+
+	@after_kickoff # Optional hook to be executed after the crew has finished
+	def log_results(self, output):
+		# Example of logging results, dynamically changing the output
+		print(f"Results: {output}")
+		return output
+
+	@agent
+	def retrieve_news(self) -> Agent:
+		return Agent(
+			config=self.agents_config['retrieve_news'],
+			tools=[SerperDevTool()], # Use this tool to ask to google to retrieve news about a specific topic
+			verbose=True,
+			llm = self.ollama_llm
+		)
+
+	@agent
+	def website_scraper(self) -> Agent:
+		return Agent(
+			config=self.agents_config['website_scraper'],
+			tools =[ScrapeWebsiteTool()],
+			verbose=True,
+			llm = self.ollama_llm
+		)
+
+	@agent
+	def ai_news_writer(self) -> Agent:
+		return Agent(
+			config=self.agents_config['ai_news_writer'],
+			tools =[],
+			verbose=True,
+			llm = self.ollama_llm
+		)
+
+	@agent
+	def file_writer(self) -> Agent:
+		return Agent(
+			config=self.agents_config['file_writer'],
+			tools =[FileWriterTool()],
+			verbose=True,
+			llm = self.ollama_llm
+		)
+
+	@task
+	def retrieve_news_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['retrieve_news_task'],
+		)
+
+	@task
+	def website_scrape_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['website_scrape_task'],
+		)
+
+	@task
+	def ai_news_write_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['ai_news_write_task'],
+		)
+
+	@task
+	def file_write_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['file_write_task'],
+		)
+
+
+	@crew
+	def crew(self) -> Crew:
+		"""Creates the SearchWeb crew"""
+		return Crew(
+			agents=self.agents, # Automatically created by the @agent decorator
+			tasks=self.tasks, # Automatically created by the @task decorator
+			process=Process.sequential,
+			verbose=True,
+			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+		)
---
+++ search_web/src/search_web/main.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import sys
+import warnings
+from datetime import datetime
+
+from search_web.crew import SearchWeb
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'topic': 'Olivier Potmans',
+        'date': datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    }
+    SearchWeb().crew().kickoff(inputs=inputs)
+
+run()
+
+
+# def train():
+#     """
+#     Train the crew for a given number of iterations.
+#     """
+#     inputs = {
+#         "topic": "Olivier Potmans"
+#     }
+#     try:
+#         SearchWeb().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+#     except Exception as e:
+#         raise Exception(f"An error occurred while training the crew: {e}")
+
+# def replay():
+#     """
+#     Replay the crew execution from a specific task.
+#     """
+#     try:
+#         SearchWeb().crew().replay(task_id=sys.argv[1])
+
+#     except Exception as e:
+#         raise Exception(f"An error occurred while replaying the crew: {e}")
+
+# def test():
+#     """
+#     Test the crew execution and returns the results.
+#     """
+#     inputs = {
+#         "topic": "AI LLMs"
+#     }
+#     try:
+#         SearchWeb().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+#     except Exception as e:
+#         raise Exception(f"An error occurred while replaying the crew: {e}")
